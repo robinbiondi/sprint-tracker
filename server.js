d4a4f3dfd2acf1a46b0de7e8c4e1cd42e419e20c
robin biondi <robin.bion@gmail.com>
2017-07-30 18:49:50 +0200

    fix config for prod

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/', function (req, res) {
 
 app.use('/client', express.static(__dirname + '/client'));
 
-app.listen(3000, function () {
+app.listen(80, function () {
   console.log('Example app listening on port 3000!');
 });
